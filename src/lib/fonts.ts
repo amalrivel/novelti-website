3fbef34b9273daaacfd6934826d57ba63b4776a4
amalrivel <amalrivel@gmail.com>
2024-06-21 18:09:50 +0700

    initial commit & feat: create navbar

---
+++ src/lib/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter as FontSans, Lusitana } from "next/font/google";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
