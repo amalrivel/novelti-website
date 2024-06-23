a53a1e2f101790c0357c9e9e11f0c661e36fe9e8
amalrivel <amalrivel@gmail.com>
2024-06-23 02:21:42 +0000

    feat:creating navbar & welcome section

---
+++ .idx/dev.nix
@@ -0,0 +1,52 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+    pkgs.bun
+  ];
+  # Sets environment variables in the workspace
+  env = { };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "aaron-bond.better-comments"
+      "aliariff.auto-add-brackets"
+      "bradlc.vscode-tailwindcss"
+      "christian-kohler.path-intellisense"
+      "dbaeumer.vscode-eslint"
+      "dsznajder.es7-react-js-snippets"
+      "EditorConfig.EditorConfig"
+      "esbenp.prettier-vscode"
+      "formulahendry.auto-close-tag"
+      "formulahendry.auto-complete-tag"
+      "formulahendry.auto-rename-tag"
+      "hbenl.test-adapter-converter"
+      "hbenl.vscode-test-explorer"
+      "mikestead.dotenv"
+      "PKief.material-icon-theme"
+      "Prisma.prisma"
+      "yzhang.markdown-all-in-one"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        bun-install = "bun ci --no-audit --prefer-offline --no-progress --timing";
+      };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [ "bun" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0" ];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,20 @@
 
 import * as React from "react";
 import Link from "next/link";
-import { lusitana } from "@/lib/fonts";
+import Image from "next/image";
 import { cn } from "@/lib/utils";
+import { lusitana } from "@/lib/fonts";
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -23,97 +23,146 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Mail } from "lucide-react";
+import { Instagram } from 'lucide-react';
+import styles from "@/app/beranda.module.css";
 
 export default function Home() {
   const { setTheme } = useTheme();
   return (
     <>
-      <nav className="container mx-auto p-4 flex justify-between items-center">
-        <Link href="/">
-          <h1 className={cn("h1", lusitana.className)}>NOVELTI</h1>
-        </Link>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Tentang Saya</NavigationMenuTrigger>
-              <NavigationMenuContent>
-              <ul className="grid w-[200px] p-4 md:w-[250px] md:grid-cols-1 lg:w-[300px] ">
-                  {components.map((component) => (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {/* {component.description} */}
-                    </ListItem>
-                  ))}
+      <nav className="sticky top-0 z-50 bg-background">
+        <div className="container mx-auto px-4 py-2 flex justify-between items-center gap-4 ">
+          <ul className="hidden justify-start items-center sm:flex">
+            <li>
+              <Button
+                variant="link"
+                size="sm"
+                className="text-accent-foreground hover:text-primary"
+                asChild
+              >
+                <Link href="/">Beranda</Link>
+              </Button>
+            </li>
+            <li>
+              <Button
+                variant="link"
+                size="sm"
+                className="text-accent-foreground hover:text-primary"
+                asChild
+              >
+                <Link href="/tentang">Tentang Saya</Link>
+              </Button>
+            </li>
+            <li>
+              <Button
+                variant="link"
+                size="sm"
+                className="text-accent-foreground hover:text-primary"
+                asChild
+              >
+                <Link href="/publikasi">Publikasi</Link>
+              </Button>
+            </li>
+            <li>
+              <Button
+                variant="link"
+                size="sm"
+                className="text-accent-foreground hover:text-primary"
+                asChild
+              >
+                <Link href="/media">Media</Link>
+              </Button>
+            </li>
+            <li>
+              <Button
+                variant="link"
+                size="sm"
+                className="text-accent-foreground hover:text-primary"
+                asChild
+              >
+                <Link href="/blog">Blog</Link>
+              </Button>
+            </li>
+            <li>
+              <Button
+                variant="link"
+                size="sm"
+                className="text-accent-foreground hover:text-primary"
+                asChild
+              >
+                <Link href="/kontak">Kontak</Link>
+              </Button>
+            </li>
+          </ul>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="relative sm:hidden"
+              >
+                <Menu />
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle className={lusitana.className}>NOVELTI</SheetTitle>
+                <ul className="flex flex-col justify-start">
+                  <li>
+                    <Button variant="ghost" asChild>
+                      <Link href="/" className="w-full h-full">
+                        Beranda
+                      </Link>
+                    </Button>
+                  </li>
+                  <li>
+                    <Button variant="ghost" asChild>
+                      <Link href="/tentang" className="w-full h-full">
+                        Tentang Saya
+                      </Link>
+                    </Button>
+                  </li>
+                  <li>
+                    <Button variant="ghost" asChild>
+                      <Link href="/publikasi" className="w-full h-full">
+                        Publikasi
+                      </Link>
+                    </Button>
+                  </li>
+                  <li>
+                    <Button variant="ghost" asChild>
+                      <Link href="/media" className="w-full h-full">
+                        Media
+                      </Link>
+                    </Button>
+                  </li>
+                  <li>
+                    <Button variant="ghost" asChild>
+                      <Link href="/blog" className="w-full h-full">
+                        Blog
+                      </Link>
+                    </Button>
+                  </li>
+                  <li>
+                    <Button variant="ghost" asChild>
+                      <Link href="/kontak" className="w-full h-full">
+                        Kontak
+                      </Link>
+                    </Button>
+                  </li>
                 </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[400px] p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  {components.map((component) => (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {/* {component.description} */}
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/docs" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Documentation
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-        <div>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="outline" size="icon">
@@ -124,44 +173,130 @@ export default function Home() {
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
               <DropdownMenuItem onClick={() => setTheme("light")}>
-                Light
+                Terang
               </DropdownMenuItem>
               <DropdownMenuItem onClick={() => setTheme("dark")}>
-                Dark
+                Gelap
               </DropdownMenuItem>
               <DropdownMenuItem onClick={() => setTheme("system")}>
-                System
+                Sistem
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
       </nav>
+      <main>
+        <section
+          className={cn("block relative bg-primary pb-32 pt-12", styles.hero)}
+        >
+          <div className="container px-4 mx-auto md:w-1/2 w-full h-full">
+            <div className="text-primary-foreground text-center w-full h-full">
+              <span>Halo semua, perkenalkan saya</span>
+              <h1 className="h1 my-4">Novelti</h1>
+              <p className="pb-2">
+                Saya seorang Dosen di Universitas Muhammadiyah Sumatera Barat
+                asdasdasd as da sad sa d asd as d as das.d.a d.as d.as d.as da.s
+                da.sd a.s d.as d.as d.as d.as d.as d.asd a.sd a.d .a d asdasd as
+                da sd as da sd as d ad as d asd. asd as da sda sd as d asd as d
+                da sd sd as da sd as d asd as d asd as da da d as das d as d asd
+                as d asd a sd asd as d as d asd as d asd as d as d asd as
+              </p>
+              <div className="flex justify-center items-center gap-4 pb-8">
+                <Button variant="link" className="text-primary-foreground" asChild>
+                  <Link href="mailto:bundonovelti@gmail.com">
+                    <Mail className="mr-2 h-4 w-4" />bundonovelti@gmail.com
+                  </Link>
+                </Button>
+                <Button variant="link" className="text-primary-foreground" asChild>
+                  <Link href="https://www.instagram.com/noveltimuis/" target="_blank">
+                    <Instagram className="mr-2 h-4 w-4" />noveltimuis
+                  </Link>
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="absolute w-full text-center z-10">
+            <Image
+              src="/PmPGYHR.png"
+              width={200}
+              height={200}
+              alt="Picture of the author"
+              className="inline rounded-full border-4 border-foreground shadow-sm h-[200px] w-[200px] object-cover"
+            />
+          </div>
+        </section>
+
+        <section className="container px-4 mx-auto md:w-3/4 w-full h-full mt-32">
+          <div className="flex flex-col items-center text-center w-full h-full">
+            <small className="muted">Tentang Saya</small>
+            <h2 className="h2 mt-3 md:w-3/5">
+              Halo, Saya <span className="text-primary">Novelti</span>. Seorang
+              Dosen Bahasa Indonesia.
+            </h2>
+          </div>
+          <div className="flex flex-row flex-wrap md:justify-center md:mt-4 text-justify">
+            <p className="basis-full md:basis-1/2 p-2">
+              Im currently the VP of Developer Experience at Vercel, where I
+              lead our Developer Relations and Documentation teams. I focus on
+              educating and growing the Vercel and Next.js communities.
+            </p>
+            <p className="basis-full md:basis-1/2 p-2">
+              I love building for the web. From something as simple as a single
+              HTML file all the way to large Next.js applications. The web is
+              incredible.
+            </p>
+            <Button className="basis-full md:basis-0 mt-4 md:mt-8">
+              Pelajari lebih lanjut
+            </Button>
+          </div>
+        </section>
+
+        <section className="container mx-auto px-4 flex flex-col-reverse items-center md:flex-row gap-4 mt-16 lg:mt-28">
+          <div className="h-full w-full">
+            <h2 className="h2">Bla Bla Bla Bla</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              ullamcorper venenatis purus in sagittis. Sed pellentesque, nibh a
+              aliquam porttitor, nisl purus lobortis nibh, vel ornare diam diam
+              ac est. Aliquam erat volutpat. Sed vehicula mi ut nisl interdum,
+              tempus aliquet est faucibus. Donec sollicitudin scelerisque diam,
+              a cursus nisi elementum vel. Donec venenatis pharetra posuere.
+              Mauris lobortis ex et libero finibus, id egestas libero blandit.
+              Nullam id scelerisque orci.
+            </p>
+          </div>
+          <Link href="/blog" className="relative h-full w-full">
+            <div className="w-full scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100 animate-shadow absolute max-w-[350px] transform-gpu top-1/2 translate-y-[-55%] md:translate-y-[-50%] lg:translate-y-[-60%] left-1/2 -translate-x-1/2 md:translate-x-[-50%] lg:translate-x-[-30%] rotate-3 md:rotate-6 lg:rotate-12 pointer-events-none md:pointer-events-auto">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Card Title</CardTitle>
+                  <CardDescription>Card Description</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                  <p>Card Footer</p>
+                </CardFooter>
+              </Card>
+            </div>
+            <div className="w-full Zscale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100 animate-shadow mx-auto max-w-[350px]">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Card Title</CardTitle>
+                  <CardDescription>Card Description</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                  <p>Card Footer</p>
+                </CardFooter>
+              </Card>
+            </div>
+          </Link>
+        </section>
+      </main>
     </>
   );
 }
-
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
-ListItem.displayName = "ListItem";
