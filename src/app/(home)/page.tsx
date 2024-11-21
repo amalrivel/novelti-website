58550ea3fc816b8b63aac43ebcf264ff532a63b1
amalrivel <amalrivel@gmail.com>
2024-11-21 10:45:57 +0700

    feat: create header and footer for home layout

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <>
+    <div>
+      <div className="container">
+        <h1 className="text-4xl font-bold text-center text-primary">
+          NOVELTI MUIS
+        </h1>
+        <p className="text-center text-lg text-secondary">
+          Novelty Management Information System
+        </p>
+      </div>
+    </div>
+    </>
+  );
+}
